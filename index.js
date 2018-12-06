58a6f6504ffdd61c94dd25b763ec2eccd6d62330
LorisC <loris@ciprin.fr>
2018-12-06 14:01:29 +0100

    [UPDATE] integrate spinal graph service

--- index.js
+++ index.js
@@ -1,82 +1,67 @@
+import { SpinalGraphService } from "spinal-env-viewer-graph-service";
 import {
-  SpinalNode,
-  SPINAL_RELATION_PTR_LST_TYPE,
-} from 'spinalgraph';
-import {
-  SpinalURL,
-  SpinalAttribute
+  SpinalAttribute,
+  SpinalURL
 } from 'spinal-models-documentation';
+
 class DocumentationService {
 
-  addNote(context, parentNode, note, name) {
+  addNote( context, parentNode, note, name ) {
     // this.getContext("Note");
-    console.log(context, parentNode, note, name)
+    console.log( context, parentNode, note, name );
   }
-  addURL(parentNode, nameURL, URL) {
-    if (nameURL != undefined && URL != undefined && URL != "" && nameURL !=
-      "") {
-      let myChild = new SpinalURL(nameURL, URL);
 
-      if (parentNode instanceof SpinalNode)
-        parentNode.addChild(myChild, 'hasURL',
-          SPINAL_RELATION_PTR_LST_TYPE);
-      else {
-        console.log("bad request add url")
-
-      }
+  addURL( parentNode, nameURL, URL ) {
+    if (nameURL != undefined && URL != undefined && URL != "" && nameURL != "") {
+      let myChild = new SpinalURL( nameURL, URL );
+      //TODO CHECK relation type. RElation type wanted SPINAL_RELATION_PTR_LST_TYPE
+      SpinalGraphService.addChildAndCreateNode( parentNode.id.get(), myChild, "hasURL", 2 );
     }
 
   }
 
-  getURL(parentNode) {
+  getURL( parentNode ) {
     let tab = [];
-    return parentNode
-      .getChildren(['hasURL'])
-      .then(myURLList => {
+    return SpinalGraphService
+      .getChildren( parentNode.id.get(), ['hasURL'] )
+      .then( myURLList => {
         // console.log(myURLList);
         for (let i = 0; i < myURLList.length; i++) {
           const urlNode = myURLList[i];
           // console.log(urlNode);
-          urlNode.element.ptr.load(url => {
-            tab.push(url);
-          });
+          urlNode.element.ptr.load( url => {
+            tab.push( url );
+          } );
         }
-      })
-      .then(() => tab);
+      } )
+      .then( () => tab );
   }
 
-  addAttribute(parentNode, label, value) {
+  addAttribute( parentNode, label, value ) {
     // this.getContext("Attribute");
     if (label != undefined && value != undefined && value != "" && label !=
       "") {
-      let myChild = new SpinalAttribute(label, value);
-
-      if (parentNode instanceof SpinalNode)
-        parentNode.addChild(
-          myChild,
-          'hasAttributes',
-          SPINAL_RELATION_PTR_LST_TYPE
-        );
-    } else {
-      console.log("bad request add attributes")
+      let myChild = new SpinalAttribute( label, value );
+      SpinalGraphService.addChild( parentNode.id.get(), myChild, 'hasAttributes', 2 );
     }
   }
 
-  getAttributes(parentNode) {
+  getAttributes( parentNode ) {
     let tab = [];
-    return parentNode
-      .getChildren(['hasAttributes'])
-      .then(myAttributesList => {
+    return SpinalGraphService
+      .getChildren( parentNode.id.get(), ['hasAttributes'] )
+      .then( myAttributesList => {
         // console.log(myAttributesList);
         for (let i = 0; i < myAttributesList.length; i++) {
           const urlNode = myAttributesList[i];
           // console.log(urlNode);
-          urlNode.element.ptr.load(url => {
-            tab.push(url);
-          });
+          urlNode.element.ptr.load( url => {
+            tab.push( url );
+          } );
         }
-      })
-      .then(() => tab);
+      } )
+      .then( () => tab );
   }
 }
-export const serviceDocumentation = new DocumentationService()
+
+export const serviceDocumentation = new DocumentationService();
