1a4a31177ce5ebf66ea73e70774297e347b1ea69
waiss67 <baqui.waiss@gmail.com>
2018-12-10 16:21:35 +0100

    update doc service

--- index.js
+++ index.js
@@ -10,49 +10,58 @@ import {
 
 class DocumentationService {
 
-  addNote( context, parentNode, note, name ) {
+  addNote(context, parentNode, note, name) {
     // this.getContext("Note");
-    console.log( context, parentNode, note, name );
+    console.log(context, parentNode, note, name);
   }
 
-  addURL( parentNode, nameURL, URL ) {
-    console.log( "url", parentNode );
+  addURL(parentNode, nameURL, URL) {
+    // console.log("url", parentNode);
+    console.log("service add url")
 
-    if (nameURL != undefined && URL != undefined && URL != "" && nameURL != "") {
-      let myChild = new SpinalURL( nameURL, URL );
+    if (nameURL != undefined && URL != undefined && URL != "" && nameURL !=
+      "") {
+      let myChild = new SpinalURL(nameURL, URL);
+      console.log("ADD URL OF DOC SERVICE", myChild)
       //TODO CHECK relation type. RElation type wanted SPINAL_RELATION_PTR_LST_TYPE
-      SpinalGraphService.addChildAndCreateNode( parentNode.getId().get(), myChild, "hasURL", SPINAL_RELATION_PTR_LST_TYPE );
+      SpinalGraphService.addChildAndCreateNode(parentNode.id.get(),
+        myChild, "hasURL", SPINAL_RELATION_PTR_LST_TYPE);
     }
   }
 
 
-  async getURL( parentNode ) {
-    const children = await SpinalGraphService.getChildren( parentNode.id.get(), ['hasURL'] );
+  async getURL(parentNode) {
+    const children = await SpinalGraphService.getChildren(parentNode.id.get(),
+      ['hasURL']);
     const res = [];
     for (let child of children) {
-      res.push( child.element.load() );
+      res.push(child.element.load());
     }
-    return Promise.all( res );
+    return Promise.all(res);
   }
 
-  async getAttributes( parentNode ) {
+  async getAttributes(parentNode) {
     const res = [];
-    const children = await SpinalGraphService.getChildren( parentNode.getId().get(), ['hasAttributes'] );
+    console.log(parentNode)
+    const children = await SpinalGraphService.getChildren(parentNode.info.id.get(),
+      ['hasAttributes']);
 
     for (let child of children) {
-      res.push( child.element.load() );
+      res.push(child.element.load());
     }
-    return Promise.all( res );
+    return Promise.all(res);
   }
 
-  addAttribute( parentNode, label, value ) {
+  addAttribute(parentNode, label, value) {
     // this.getContext("Attribute");
-    console.log( parentNode );
-
+    // console.log( parentNode );
+    console.log(parentNode)
     if (label != undefined && value != undefined && value != "" && label !=
       "") {
-      let myChild = new SpinalAttribute( label, value );
-      SpinalGraphService.addChild( parentNode.getId().get(), myChild, 'hasAttributes', SPINAL_RELATION_PTR_LST_TYPE );
+      let myChild = new SpinalAttribute(label, value);
+      console.log(parentNode)
+      SpinalGraphService.addChild(parentNode.info.id.get(), myChild,
+        'hasAttributes', SPINAL_RELATION_PTR_LST_TYPE);
     }
   }
 
