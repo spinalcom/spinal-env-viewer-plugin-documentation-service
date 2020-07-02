f79313f285bb8a30a6402e4ac304a14d7219e4a8
laurent <htd.laurent@gmail.com>
2020-07-02 19:45:34 +0200

    add dependency and remove es6 syntax form index

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
+/*
+ * Copyright 2020 SpinalCom - www.spinalcom.com
+ * 
+ * This file is part of SpinalCore.
+ * 
+ * Please read all of the following terms and conditions
+ * of the Free Software license Agreement ("Agreement")
+ * carefully.
+ * 
+ * This Agreement is a legally binding contract between
+ * the Licensee (as defined below) and SpinalCom that
+ * sets forth the terms and conditions that govern your
+ * use of the Program. By installing and/or using the
+ * Program, you agree to abide by all the terms and
+ * conditions stated or referenced herein.
+ * 
+ * If you do not agree to abide by these terms and
+ * conditions, do not demonstrate your acceptance and do
+ * not install or use the Program.
+ * You should have received a copy of the license along
+ * with this file. If not, see
+ * <http://resources.spinalcom.com/licenses.pdf>.
+ */
+
 const {
   SpinalNode,
   SPINAL_RELATION_PTR_LST_TYPE
@@ -11,7 +35,8 @@ const {
   SpinalNote,
 } = require("spinal-models-documentation");
 var $q = require("q");
-import bimObjectService from "spinal-env-viewer-plugin-bimobjectservice";
+
+const bimObjectService = require("spinal-env-viewer-plugin-bimobjectservice").default
 const {
   groupService
 } = require("spinal-env-viewer-room-manager/services/service");
